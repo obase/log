406497d60beb94cfb0a15c81f322b4f8a83f6367
hezhaowu <hezhaowu@kingsoft.com>
2019-05-21 11:41:33 +0800

    提交pbx项目

---
+++ def.go
@@ -0,0 +1,15 @@
+package log
+
+import (
+	"context"
+)
+
+/*日志适配方法*/
+type LoggerFunc func(ctx context.Context, format string, args ...interface{})
+
+// 全局日志输出适配, 应用可以写日志前覆盖此值
+var (
+	Errorf LoggerFunc = Error
+	Inforf LoggerFunc = Info
+	Debugf LoggerFunc = Debug
+)
