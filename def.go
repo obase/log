2886c9aeff7d3e65f30c305c2385af767091239e
hezhaowu <hezhaowu@kingsoft.com>
2019-06-17 23:57:45 +0800

    upgrade to v0.10.2

--- def.go
+++ def.go
@@ -10,7 +10,9 @@ type LoggerFunc func(ctx context.Context, format string, args ...interface{})
 // 全局日志输出适配, 应用可以写日志前覆盖此值
 var (
 	Errorf LoggerFunc = Error
+	// - Deprecated
 	Inforf LoggerFunc = Info
+	Infof  LoggerFunc = Info
 	Warnf  LoggerFunc = Warn
 	Debugf LoggerFunc = Debug
 	Flushf func()     = Flush
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/obase/log
 
 go 1.12
 
-require github.com/obase/conf v0.10.0
+require github.com/obase/conf v0.10.2
