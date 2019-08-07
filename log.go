592c99fa35bd5ff97031191be290ef453a552702
hezhaowu <hezhaowu@kingsoft.com>
2019-08-07 20:59:09 +0800

    save

--- log.go
+++ log.go
@@ -107,6 +107,12 @@ func Setup(flushPeriod time.Duration, opts ...*Config) (err error) {
 			Warn = _default.Warn
 			Info = _default.Info
 			Debug = _default.Debug
+
+			Fatalf = _default.Fatal
+			Errorf = _default.Error
+			Warnf = _default.Warn
+			Infof = _default.Info
+			Debugf = _default.Debug
 		}
 	}()
 
